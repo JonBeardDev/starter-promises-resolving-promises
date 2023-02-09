625d5964eb96c230b944704d25b0e6ed1b4505e7
JonBeardDev <jon.beard@gmail.com>
2023-02-09 13:46:01 -0600

    .then/.catch changes from lesson

--- src/main.js
+++ src/main.js
@@ -3,3 +3,22 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Will the weather be nice today?";
+const tellPromise = tell(question);
+tellPromise.then((fortune) => {
+  console.log(question);
+  console.log(fortune);
+});
+
+//> Will the weather be nice today?
+//> Without a doubt. (or other random response)
+
+tell()
+  .then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+  })
+  .catch(console.error);
+
+// No question is being passed into tell, so the error message will be 'caught'
